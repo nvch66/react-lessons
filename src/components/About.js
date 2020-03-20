f19819932d48e43bc4423b8ed1f4047c33760851
nvch66 <nvch66@gmail.com>
2020-03-20 22:03:09 +0530

    Changed About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return(
         <div className="container">
             <h3 className="center">About Page</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magni explicabo exercitationem nesciunt, magnam earum inventore at eligendi velit maxime, nostrum nemo veritatis officiis adipisci dolor placeat harum, modi accusamus?</p>
+            <p>Hello Guys, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magni explicabo exercitationem nesciunt, magnam earum inventore at eligendi velit maxime, nostrum nemo veritatis officiis adipisci dolor placeat harum, modi accusamus?</p>
         </div>
     )
 }
